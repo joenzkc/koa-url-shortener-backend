3d9d59d6eac46b03d24787b1a0e0f6f80ed82f22
joenz <>
2023-05-19 23:06:24 +0800

    commit

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const router: Router = new Router();
 app.use(bodyParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: process.env.FRONTEND_URL,
   })
 );
 router.get(
